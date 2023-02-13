8bd82a923cf3cf867c564893306a43bf7deedf2f
Nikola Kozhuharov <nikola.kozhuharov@waterdrop.com>
2023-02-13 15:43:57 +0100

    Refactor UserController to allow unit tests

--- users/src/Controller/UserController.php
+++ users/src/Controller/UserController.php
@@ -4,26 +4,34 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Message\NewUserMessage;
-use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserController extends AbstractController
 {
-
-    public function __construct(protected ValidatorInterface $validator)
+    public function __construct(
+        protected ValidatorInterface  $validator,
+        protected MessageBusInterface $bus,
+        protected UserRepository      $repository,
+        protected RequestStack        $requestStack
+    )
     {
     }
 
     #[Route('/users', name: 'create_user')]
-    public function createProduct(MessageBusInterface $bus, ManagerRegistry $doctrine, Request $request): JsonResponse
+    /**
+     * Validate the incoming request.
+     * Create a new User in the database.
+     * Send a NewUserMessage to the RabbitMQ.
+     */
+    public function createUser(): JsonResponse
     {
-        $entityManager = $doctrine->getManager();
-
+        $request = $this->requestStack->getCurrentRequest();
         $user = new User();
         $user->setEmail($request->get('email'));
         $user->setFirstName($request->get('firstName'));
@@ -37,14 +45,12 @@ class UserController extends AbstractController
                 $response[$error->getPropertyPath()] = $error->getMessage();
             }
 
-            return new JsonResponse($response);
+            return new JsonResponse($response, 422);
         }
 
-        $entityManager->persist($user);
+        $this->repository->save($user, true);
 
-        $entityManager->flush();
-
-        $bus->dispatch(new NewUserMessage($user));
+        $this->bus->dispatch(new NewUserMessage($user));
 
         return new JsonResponse($user->toArray());
     }
